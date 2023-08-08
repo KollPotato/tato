28461edc08e80089c246f32db1954df22667a412
Koll <kollpotato@gmail.com>
2023-08-08 19:39:03 +0300

    refactor ChatGPT's code a bit

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,17 @@
 export function levenshteinDistance(string1: string, string2: string): number {
-    const string1_length = string1.length
-    const string2_length = string2.length
     const dp: number[][] = []
 
-    for (let i = 0; i <= string1_length; i++) {
+    for (let i = 0; i <= string1.length; i++) {
         dp[i] = [];
         dp[i][0] = i;
     }
 
-    for (let j = 0; j <= string2_length; j++) {
+    for (let j = 0; j <= string2.length; j++) {
         dp[0][j] = j;
     }
 
-    for (let i = 1; i <= string1_length; i++) {
-        for (let j = 1; j <= string2_length; j++) {
+    for (let i = 1; i <= string1.length; i++) {
+        for (let j = 1; j <= string2.length; j++) {
             const cost = string1[i - 1] === string2[j - 1] ? 0 : 1
             dp[i][j] = Math.min(
                 dp[i - 1][j] + 1,
@@ -23,7 +21,7 @@ export function levenshteinDistance(string1: string, string2: string): number {
         }
     }
 
-    return dp[string1_length][string2_length]
+    return dp[string1.length][string2.length]
 }
 
 export function findClosestMatch(input: string, allowedStrings: string[], threshold: number): string | null {
