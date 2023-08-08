0a76f313f3ce31ff1973d272c17bc2b9dd88b750
Koll <kollpotato@gmail.com>
2023-08-08 19:39:35 +0300

    re-export language's core features

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,5 @@
-import { Stack, VirtualMachine } from "$vm"
-import { Compiler } from "$compiler"
-import { InputStream, TokenStream, Parser } from "$parser"
-
-const input = `
-println(true == false,)
-
-`
-
-const inputStream = new InputStream(input)
-const tokenStream = new TokenStream(inputStream)
-
-const parser = new Parser(tokenStream)
-const program = parser.parseProgram()
-
-const compiler = new Compiler(program)
-const instructions = compiler.compileProgram()
-
-const vm = new VirtualMachine({
-    instructions,
-    stack: new Stack(12)
-})
-
-vm.execute()
+export * from "$vm"
+export * from "$parser"
+export * from "$compiler"
+export * from "$errors"
+export * from "$shared/operators"
